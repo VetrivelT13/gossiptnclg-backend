6222f2085a3e2e39fdf1c04c8a9b0d9e2419409c
Vetrivel-2005 <pract.8080@gmail.com>
2025-12-20 01:01:02 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js - Complete Backend for GossipTNClg
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const http = require('http');
+const socketIo = require('socket.io');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }
+});
